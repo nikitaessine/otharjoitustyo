041b3490d7d82d66793bc1d0d9b735d38498d797
Nikita Essine <nikita.essine@helsinki.fi>
2021-12-02 12:21:25 +0200

    more tasks added

--- tasks.py
+++ tasks.py
@@ -19,3 +19,11 @@ def coverage(ctx):
 @task()
 def coverage_report(ctx):
     ctx.run("coverage html")
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
+
+@task
+def forma(ctx):
+    ctx.run("format")
